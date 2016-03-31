0d547eb26b84bac337f9030437de82876de2feae
developframework <408000511@qq.com>
2016-03-31 15:06:48 +0800

    测试项

---
+++ jsonview3-spring-support/src/test/java/test/JunitTest.java
@@ -0,0 +1,44 @@
+package test;
+
+import javax.annotation.Resource;
+
+import org.developframework.jsonview.core.JsonCreator;
+import org.developframework.jsonview.core.JsonviewFactory;
+import org.developframework.jsonview.data.DataModel;
+import org.developframework.jsonview.data.HashDataModel;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
+public class JunitTest {
+
+	@Resource
+	private JsonviewFactory jsonviewFactory;
+
+	private User[] users;
+
+	@Before
+	public void init() {
+		User user1 = new User(1, "zhangsan");
+		User user2 = new User(2, "lisi");
+		users = new User[]{
+				user1, user2
+		};
+	}
+
+	@Test
+	public void test() {
+		JsonCreator jsonCreator = jsonviewFactory.getJsonCreator();
+		DataModel dataModel = new HashDataModel();
+		dataModel.putData("user", users[0]);
+		dataModel.putData("users", users);
+		String json = jsonCreator.createJson(dataModel, "jsonview-demo", "user-list");
+		System.out.println(json);
+
+	}
+
+}
--- jsonview3-spring-support/src/test/java/test/User.java
+++ jsonview3-spring-support/src/test/java/test/User.java
@@ -1,8 +1,6 @@
 package test;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 public class User {
 
@@ -12,14 +10,11 @@ public class User {
 	private Like[] likes = new Like[]{
 			new Like("aaa"), new Like("bbb")
 	};
-	private Map<String, String> map = new HashMap<String, String>();
 
 	public User(int id, String name) {
 		this.id = id;
 		this.name = name;
 		this.birthday = new Date();
-		map.put("key1", "value1");
-		map.put("key2", "value2");
 	}
 
 	public int getId() {
@@ -44,14 +39,6 @@ public class User {
 
 	public void setLikes(Like[] likes) {
 		this.likes = likes;
-	}
-
-	public Map<String, String> getMap() {
-		return map;
-	}
-
-	public void setMap(Map<String, String> map) {
-		this.map = map;
 	}
 
 	public Date getBirthday() {
