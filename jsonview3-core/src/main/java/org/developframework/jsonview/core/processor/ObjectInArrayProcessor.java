67b461011726eb291597e60078cdbd134a26ee69
developframework <408000511@qq.com>
2016-04-07 15:52:30 +0800

    记录index

--- jsonview3-core/src/main/java/org/developframework/jsonview/core/processor/ObjectInArrayProcessor.java
+++ jsonview3-core/src/main/java/org/developframework/jsonview/core/processor/ObjectInArrayProcessor.java
@@ -4,12 +4,20 @@ import org.developframework.jsonview.core.element.ObjectElement;
 
 public class ObjectInArrayProcessor extends ObjectProcessor {
 
+	private int index;
+
 	public ObjectInArrayProcessor(Context context, ObjectElement element, String parentExpression, int index) {
 		super(context, element, parentExpression + "[" + index + "]");
+		this.index = index;
 	}
 
 	@Override
 	public String createExpression(String parentExpression) {
 		return parentExpression;
 	}
+
+	public int getIndex() {
+		return index;
+	}
+
 }
