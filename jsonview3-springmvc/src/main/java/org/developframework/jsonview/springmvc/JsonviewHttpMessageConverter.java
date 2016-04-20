59dddc6e25e016cee74d3833ede614429612e1fc
developframework <408000511@qq.com>
2016-04-20 14:50:12 +0800

    添加空构造方法

--- jsonview3-springmvc/src/main/java/org/developframework/jsonview/springmvc/JsonviewHttpMessageConverter.java
+++ jsonview3-springmvc/src/main/java/org/developframework/jsonview/springmvc/JsonviewHttpMessageConverter.java
@@ -24,22 +24,40 @@ public class JsonviewHttpMessageConverter extends MappingJackson2HttpMessageConv
 
 	protected JsonviewFactory jsonviewFactory;
 
+	public JsonviewHttpMessageConverter() {
+
+	}
+
 	public JsonviewHttpMessageConverter(JsonviewFactory jsonviewFactory) {
 		this.jsonviewFactory = jsonviewFactory;
 	}
 
 	@Override
-	public boolean canWrite(Type type, Class<?> clazz, MediaType mediaType) {
+	public boolean canWrite(Class<?> clazz, MediaType mediaType) {
 		return JsonviewResponse.class.isAssignableFrom(clazz);
 	}
 
 	@Override
-	protected void writeInternal(Object object, Type type, HttpOutputMessage outputMessage) throws IOException, HttpMessageNotWritableException {
+	public boolean canWrite(Type type, Class<?> clazz, MediaType mediaType) {
+		return this.canWrite(clazz, mediaType);
+	}
+
+	@Override
+	protected void writeInternal(Object object, HttpOutputMessage outputMessage) throws IOException, HttpMessageNotWritableException {
 		JsonviewResponse jsonviewResponse = (JsonviewResponse) object;
 		JsonCreator creator = jsonviewFactory.getJsonCreator(super.objectMapper);
 		JsonEncoding encoding = super.getJsonEncoding(outputMessage.getHeaders().getContentType());
 		JsonGenerator generator = super.objectMapper.getFactory().createGenerator(outputMessage.getBody(), encoding);
 		creator.printJson(generator, jsonviewResponse.getDataModel(), jsonviewResponse.getNamespace(), jsonviewResponse.getJsonviewId());
+	}
+
+	@Override
+	protected void writeInternal(Object object, Type type, HttpOutputMessage outputMessage) throws IOException, HttpMessageNotWritableException {
+		this.writeInternal(object, outputMessage);
+	}
+
+	public void setJsonviewFactory(JsonviewFactory jsonviewFactory) {
+		this.jsonviewFactory = jsonviewFactory;
 	}
 
 	public JsonviewFactory getJsonviewFactory() {
